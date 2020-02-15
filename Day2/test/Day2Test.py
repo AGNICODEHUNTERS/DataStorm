bfd00c236bb5819fbed4b05eaed649ba51d20b16
AGNICODEHUNTERS <61060149+AGNICODEHUNTERS@users.noreply.github.com>
2020-02-15 14:10:01 +0530

    commit

--- Day2/test/Day2Test.py
+++ Day2/test/Day2Test.py
@@ -5,6 +5,29 @@ import seaborn as sns
 import scipy.stats as stats
 
 data = pd.read_csv("credit_card_default_test.csv")
-print(data.head(10))
 
-print(data.columns)
+
+def numeric(dataSheet):
+    bal = dataSheet.Balance_Limit_V1
+    gen = dataSheet.Gender
+    edu = dataSheet.EDUCATION_STATUS
+    mar = dataSheet.MARITAL_STATUS
+    age = dataSheet.AGE
+
+    i=0
+
+    balF = []
+    genF = []
+    eduF = []
+    marF = []
+    ageF = []
+
+    for m in range(len(bal)):
+        if bal.iloc[m].endswith('M'):
+            balF.append(float(bal[m][:-1])*1000000)
+        elif bal.iloc[m].endswith('K'):
+            balF.append(float(bal[m][:-1])*1000)
+        else:
+            balF.append(float(bal[m]))
+data=numeric(data)
+print(data)
