a32dfb2e123543329380183d61bfdf6f7b58ace1
AGNICODEHUNTERS <61060149+AGNICODEHUNTERS@users.noreply.github.com>
2020-02-15 13:25:34 +0530

    day2 test

--- Day2/test/Day2Test.py
+++ Day2/test/Day2Test.py
@@ -1,1 +1,10 @@
-s
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
+
+data = pd.read_csv("../input/UCI_Credit_Card.csv")
+data.head(10)
+
+data.columns
