2fd251ac97c7191b4bfe3adf1f66ea9db3f1ed49
iresh <agnicodehunters@gmail.com>
2020-02-15 16:49:26 +0530

    85%

--- Day2/test/Day2Test2.py
+++ Day2/test/Day2Test2.py
@@ -1,7 +1,13 @@
 import tensorflow as tf
 import pandas as pd
+import seaborn as sns
+import tensorflow_docs as tfdocs
+import tensorflow_docs.plots
+import tensorflow_docs.modeling
+import matplotlib.pyplot as plt
 
-dataSet = pd.read_csv("credit_card_default_train.csv")
+dataSetT = pd.read_csv("credit_card_default_train.csv",header=0)
+dataSetTs = pd.read_csv("credit_card_default_test.csv",header=0)
 
 def numeric(dataSheet):
     bal = dataSheet.Balance_Limit_V1
@@ -63,6 +69,42 @@ def numeric(dataSheet):
             break
     return dataSheet
 
-data=numeric(dataSet)
-#testData=numeric(testData)
-print(data)
+def normDat(dataSet):
+    return (dataSet-train_stats['mean'])/train_stats['std']
+
+def buildModel():
+    model = tf.keras.Sequential([tf.keras.layers.Dense(64,activation='relu',input_shape=[len(train_dat.keys())]),tf.keras.layers.Dense(64,activation='relu'),tf.keras.layers.Dense(1)])
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+    model.compile(loss='mse',optimizer=optimizer,metrics=['mae','mse'])
+    return model
+
+data=numeric(dataSetT)
+testData=numeric(dataSetTs)
+
+
+train_dat=data.drop(["Client_ID","Balance_Limit_V1","Gender","EDUCATION_STATUS","MARITAL_STATUS","AGE"],axis=1)
+test_dat=testData.drop(["Client_ID","Balance_Limit_V1","Gender","EDUCATION_STATUS","MARITAL_STATUS","AGE"],axis=1)
+
+
+train_lab = train_dat.pop("NEXT_MONTH_DEFAULT")
+test_lab = test_dat.pop("NEXT_MONTH_DEFAULT")
+
+train_stats=train_dat.describe()
+train_stats = train_stats.transpose()
+normed_train_dat = normDat(train_dat)
+normed_test_dat = normDat(test_dat)
+model = buildModel()
+
+EPOCHS=1000
+history = model.fit(normed_train_dat,train_lab,epochs = EPOCHS,validation_split = 0.2,verbose = 0,callbacks=[tfdocs.modeling.EpochDots()])
+loss, mae, mse = model.evaluate(normed_test_dat, test_lab, verbose=2)
+test_predictions = model.predict(normed_test_dat).flatten()
+tp=[]
+for i in test_predictions:
+    fin=int(round(i))
+    tp.append(abs(fin))
+dataSheet=pd.DataFrame()
+dataSheet.insert(0,"Client_ID",data.Client_ID)
+dataSheet.insert(1,"NEXT_MONTH_DEFAULT",pd.DataFrame(tp))
+dataSheet.to_csv(r'AGNI_CODE_HUNTERS.csv')
+print(count/len(train_lab))
