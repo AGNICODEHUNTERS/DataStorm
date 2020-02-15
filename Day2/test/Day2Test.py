f1c2ea8b0a641b665c7401521f383c3405d39b3c
AGNICODEHUNTERS <61060149+AGNICODEHUNTERS@users.noreply.github.com>
2020-02-15 17:23:10 +0530

    data analysis

--- Day2/test/Day2Test.py
+++ Day2/test/Day2Test.py
@@ -4,9 +4,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
 
-data = pd.read_csv("credit_card_default_test.csv")
+data = pd.read_csv("credit_card_default_train.csv")
+output='NEXT_MONTH_DEFAULT'
+cols = [ f for f in data.columns if data.dtypes[ f ] != "object"]
+#cols.remove('Client_ID')
+cols.remove( output )
+f = pd.melt( data, id_vars=output, value_vars=cols)
+print(f)
+g = sns.FacetGrid( f, hue=output, col="variable", col_wrap=5, sharex=False, sharey=False )
+g = g.map( sns.distplot, "value", kde=True).add_legend()
+plt.show()
 
-
+''''
 def numeric(dataSheet):
     bal = dataSheet.Balance_Limit_V1
     gen = dataSheet.Gender
@@ -29,5 +38,6 @@ def numeric(dataSheet):
             balF.append(float(bal[m][:-1])*1000)
         else:
             balF.append(float(bal[m]))
+    return dataSheet
 data=numeric(data)
-print(data)
+print(data)'''
