1f6feee32420a588f8faab65364a0419fdb52e4c
AGNICODEHUNTERS <61060149+AGNICODEHUNTERS@users.noreply.github.com>
2020-02-15 18:22:13 +0530

    data

---
+++ Day2/test/data_analysis_R.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import seaborn as sns
+
+data = pd.read_csv("credit_card_default_train.csv")
+print(data)
