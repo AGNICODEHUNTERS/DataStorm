5d8ff0f060e403b7119c852bb5c2c5c7a7e938ce
AGNICODEHUNTERS <61060149+AGNICODEHUNTERS@users.noreply.github.com>
2020-02-16 23:21:02 +0530

    comments

--- Day3/data_analysis_R.py
+++ Day3/data_analysis_R.py
@@ -3,12 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import seaborn as sns
+from sklearn import metrics
 
+data = pd.read_csv("credit_card_default_train.csv") #importing train data
+testData=pd.read_csv("credit_card_default_test.csv") #importing test data
 
-data = pd.read_csv("credit_card_default_train.csv")
-
-testData=pd.read_csv("credit_card_default_test.csv")
-
+####function for converting literal values to numeric values###
 def numeric(dataSheet):
     bal = dataSheet.Balance_Limit_V1
     gen = dataSheet.Gender
@@ -69,19 +69,24 @@ def numeric(dataSheet):
     return dataSheet
 
 data=numeric(data)
-data=data.drop(["Client_ID","Balance_Limit_V1","Gender","EDUCATION_STATUS","MARITAL_STATUS","AGE"],axis=1)
 testData=numeric(testData)
+
+
+data=data.drop(["Client_ID","Balance_Limit_V1","Gender","EDUCATION_STATUS","MARITAL_STATUS","AGE"],axis=1)
 testData=testData.drop(["Client_ID","Balance_Limit_V1","Gender","EDUCATION_STATUS","MARITAL_STATUS","AGE"],axis=1)
+
 cols = [ f for f in data.columns if data.dtypes[ f ] != "object"]
 colstest = [ f for f in testData.columns if testData.dtypes[ f ] != "object"]
 cols.remove('NEXT_MONTH_DEFAULT')
+
+###Exploratory Data Analysis###
 for i in cols:
     f = pd.melt( data, id_vars='NEXT_MONTH_DEFAULT',  value_vars=i)
     g = sns.FacetGrid( f, hue='NEXT_MONTH_DEFAULT', col="variable", col_wrap=1, sharex=False, sharey=False )
     g = g.map( sns.distplot, "value", kde=True).add_legend()
     #plt.savefig(i+".png")
 
-
+###Function for performing the ChiSquared Test Of Independence###
 def CSTOW ( inputdata, inputvariable, OutcomeCategory ):
     OutcomeCategoryTable = inputdata.groupby( OutcomeCategory )[ OutcomeCategory ].count().values
     OutcomeCategoryRatio = OutcomeCategoryTable / sum( OutcomeCategoryTable )
@@ -110,14 +115,26 @@ def CSTOW ( inputdata, inputvariable, OutcomeCategory ):
     p_value = 1 - stats.chi2.cdf(x=chi_squared_stat, df=degOfF)
     print("Calculated test-statistic is %.2f" % chi_squared_stat )
     print("If " + OutcomeCategory + " is indep of " + inputvariable + ", this has prob %.2e of occurring" % p_value )
+for i in cols:
+    CSTOW(data,i,'NEXT_MONTH_DEFAULT')
+###The quantitativeitative variables###
+quantitative = ["balF", "ageF"]
 
-# The quantitative vars:
-quant = ["balF", "ageF"]
-# The qualitative but "Encoded" variables (ie most of them)
+# The qualitative, encoded variables###
 qual_Enc = cols
 qual_Enc.remove("balF")
 qual_Enc.remove("ageF")
 logged = []
+
+### The quantitativeitative variables for testData###
+quantitative = ["balF", "ageF"]
+###The qualitative,encoded variables for testData###
+quantitative_Encoded = colstest
+quantitative_Encoded.remove("balF")
+quantitative_Encoded.remove("ageF")
+loggedtest = []
+
+###converting data into logarithmic values###
 for m in ("PAID_AMT_JULY","PAID_AMT_AUG","PAID_AMT_SEP","PAID_AMT_OCT","PAID_AMT_NOV",'PAID_AMT_DEC'):
     qual_Enc.remove(m)
     data[m]  = data[m].apply( lambda x: np.log1p(x) if (x>0) else 0 )
@@ -127,35 +144,30 @@ for n in ("DUE_AMT_JULY","DUE_AMT_AUG","DUE_AMT_SEP","DUE_AMT_OCT","DUE_AMT_NOV"
     qual_Enc.remove(n)
     data[ n] = data[n].apply( lambda x: np.log1p(x) if (x>0) else 0 )
     logged.append(n)
-###############################
-# The quantitative vars:
-quant = ["balF", "ageF"]
-# The qualitative but "Encoded" variables (ie most of them)
-qual_Enctest = colstest
-qual_Enctest.remove("balF")
-qual_Enctest.remove("ageF")
-loggedtest = []
+
+
 for m in ("PAID_AMT_JULY","PAID_AMT_AUG","PAID_AMT_SEP","PAID_AMT_OCT","PAID_AMT_NOV",'PAID_AMT_DEC'):
-    qual_Enctest.remove(m)
+    quantitative_Encoded.remove(m)
     testData[m]  = testData[m].apply( lambda x: np.log1p(x) if (x>0) else 0 )
     loggedtest.append(m)
 
 for n in ("DUE_AMT_JULY","DUE_AMT_AUG","DUE_AMT_SEP","DUE_AMT_OCT","DUE_AMT_NOV","DUE_AMT_DEC"):
-    qual_Enctest.remove(n)
+    quantitative_Encoded.remove(n)
     testData[ n] = testData[n].apply( lambda x: np.log1p(x) if (x>0) else 0 )
     loggedtest.append(n)
 
-################################
+###Exploratory Data Analysis for the logarithmic values###
 for i in logged:
     f = pd.melt( data, id_vars='NEXT_MONTH_DEFAULT', value_vars=i)
     g = sns.FacetGrid( f, hue='NEXT_MONTH_DEFAULT', col="variable", col_wrap=1, sharex=False, sharey=False )
     g = g.map( sns.distplot, "value", kde=True).add_legend()
 
-features = quant + qual_Enc + logged + ['NEXT_MONTH_DEFAULT']
+features = quantitative + qual_Enc + logged + ['NEXT_MONTH_DEFAULT']
 corr = data[features].corr()
 plt.subplots(figsize=(30,10))
 sns.heatmap( corr, square=True, annot=True, fmt=".1f" )
 
+###Defining the dataSet for Analysis###
 '''X_train=data.drop(["NEXT_MONTH_DEFAULT"],axis=1).values
 Y_train = data["NEXT_MONTH_DEFAULT"].values
 
@@ -164,8 +176,8 @@ Y_test = data["NEXT_MONTH_DEFAULT"].values'''
 
 X= data.drop(["NEXT_MONTH_DEFAULT"],axis=1).values
 Y=data["NEXT_MONTH_DEFAULT"].values
-featurestest= quant + qual_Enctest + loggedtest
-features = quant + qual_Enc + logged
+featurestest= quantitative + quantitative_Encoded + loggedtest
+features = quantitative + qual_Enc + logged
 '''X_train= data[features].values
 X_test= testData[featurestest].values
 Y_train= data[ "NEXT_MONTH_DEFAULT" ].values'''
@@ -173,6 +185,7 @@ Y_train= data[ "NEXT_MONTH_DEFAULT" ].values'''
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size=0.2)
 
+###Scaling the data###
 from sklearn.preprocessing import StandardScaler
 scX = StandardScaler()
 X_train = scX.fit_transform( X_train )
@@ -181,93 +194,62 @@ X_test = scX.transform( X_test )
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score
 
+###Random Forest Classifier###
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators=10)
 classifier.fit( X_train, Y_train )
 Y_pred = classifier.predict( X_test )
 
+###Obtaining the acuuracy levels for Random Forest Classifier###
 cm = confusion_matrix( Y_test, Y_pred )
-print("Accuracy on Test Set for RandomForest = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
+print("Accuracy of Test Set by RandomForest = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
 scoresRF = cross_val_score( classifier, X_train, Y_train, cv=10)
-print("Mean RandomForest CrossVal Accuracy on Train Set %.2f, with std=%.2f" % (scoresRF.mean(), scoresRF.std() ))
-print(Y_pred)
+print("Mean RandomForest CrossVal Accuracy on the Train Set %.2f, with std=%.2f" % (scoresRF.mean(), scoresRF.std() ))
 
+###Kernel SVM Classifier###
 from sklearn.svm import SVC
 classifier1 = SVC(kernel="rbf")
 classifier1.fit( X_train, Y_train )
 Y_pred = classifier1.predict( X_test )
 
+###Obtaining the acuuracy levels for Kernel SVM Classifier###
 cm = confusion_matrix( Y_test, Y_pred )
-print("Accuracy on Test Set for kernel-SVM = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
+print("Accuracy of Test Set by kernel-SVM = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
 scoresSVC = cross_val_score( classifier1, X_train, Y_train, cv=10)
-print("Mean kernel-SVM CrossVal Accuracy on Train Set %.2f, with std=%.2f" % (scoresSVC.mean(), scoresSVC.std() ))
+print("Mean kernel-SVM CrossVal Accuracy on the Train Set %.2f, with std=%.2f" % (scoresSVC.mean(), scoresSVC.std() ))
 
-
+###Logistic Regression classification###
 from sklearn.linear_model import LogisticRegression
 classifier2 = LogisticRegression()
 classifier2.fit( X_train, Y_train )
 Y_pred = classifier2.predict( X_test )
-
+###Obtaining the acuuracy levels for Logistic Regression###
 cm = confusion_matrix( Y_test, Y_pred )
-print("Accuracy on Test Set for LogReg = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
+print("Accuracy of Test Set by LogReg = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
 scoresLR = cross_val_score( classifier2, X_train, Y_train, cv=10)
-print("Mean LogReg CrossVal Accuracy on Train Set %.2f, with std=%.2f" % (scoresLR.mean(), scoresLR.std() ))
+print("Mean LogReg CrossVal Accuracy on the Train Set %.2f, with std=%.2f" % (scoresLR.mean(), scoresLR.std() ))
 
+###Naive Bayes classification###
 from sklearn.naive_bayes import GaussianNB
 classifier3 = GaussianNB()
 classifier3.fit( X_train, Y_train )
 Y_pred = classifier3.predict( X_test )
+###Obtaining the acuuracy levels for Naive Bayes classification###
 cm = confusion_matrix(Y_test, Y_pred )
-print("Accuracy on Test Set for NBClassifier = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
+print("Accuracy of the Test Set by NBClassifier = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
 scoresNB = cross_val_score( classifier3, X_train, Y_train, cv=10)
-print("Mean NaiveBayes CrossVal Accuracy on Train Set %.2f, with std=%.2f" % (scoresNB.mean(), scoresNB.std() ))
+print("Mean NaiveBayes CrossVal Accuracy on the Train Set %.2f, with std=%.2f" % (scoresNB.mean(), scoresNB.std() ))
 
-
+###K-neighbors Classification###
 from sklearn.neighbors import KNeighborsClassifier
 classifier4 = KNeighborsClassifier(n_neighbors=5)
 classifier4.fit( X_train, Y_train )
 Y_pred = classifier4.predict( X_test )
+###Obtaining the acuuracy levels for K-neighbors classification###
 cm = confusion_matrix( Y_test, Y_pred )
 print("Accuracy on Test Set for KNeighborsClassifier = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
 scoresKN = cross_val_score( classifier3, X_train, Y_train, cv=10)
 print("Mean KN CrossVal Accuracy on Train Set Set %.2f, with std=%.2f" % (scoresKN.mean(), scoresKN.std() ))
-
-Ran = RandomForestClassifier(criterion= 'gini', max_depth= 6,
-                                     max_features= 5, n_estimators= 150,
-                                     random_state=0)
-Ran.fit(X_train, Y_train)
-Y_pred = Ran.predict(X_test)
-print('Accuracy:', metrics.accuracy_score(Y_pred,Y_test))
-
-## 5-fold cross-validation
-cv_scores =cross_val_score(Ran, X, Y, cv=5)
-
-# Print the 5-fold cross-validation scores
-print()
-print(classification_report(Y_test,Y_pred))
-print()
-print("Average 5-Fold CV Score: {}".format(round(np.mean(cv_scores),4)),
-      ", Standard deviation: {}".format(round(np.std(cv_scores),4)))
-
-plt.figure(figsize=(4,3))
-ConfMatrix = confusion_matrix(y_test,Ran.predict(X_test))
-sns.heatmap(ConfMatrix,annot=True, cmap="Blues", fmt="d",
-            xticklabels = ['Non-default', 'Default'],
-            yticklabels = ['Non-default', 'Default'])
-plt.ylabel('True label')
-plt.xlabel('Predicted label')
-plt.title("Confusion Matrix - Random Forest");
-
-
-
-
-
-
-
-
-
-
-
-
+###Exporting results into a csv file
 df = pd.DataFrame(Y_pred)
 df.to_csv(r'a.csv')
