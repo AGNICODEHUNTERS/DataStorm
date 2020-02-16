cf51e466afa4c04acd4ebf97c875f80829d39e7e
AGNICODEHUNTERS <61060149+AGNICODEHUNTERS@users.noreply.github.com>
2020-02-16 15:00:49 +0530

    data analysis

--- Day2/test/data_analysis_R.py
+++ Day2/test/data_analysis_R.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 data = pd.read_csv("credit_card_default_train.csv")
 
-#testData=pd.read_csv("credit_card_default_test")
+#testData=pd.read_csv("credit_card_default_test.csv")
 
 def numeric(dataSheet):
     bal = dataSheet.Balance_Limit_V1
@@ -73,7 +73,6 @@ data=numeric(data)
 data=data.drop(["Client_ID","Balance_Limit_V1","Gender","EDUCATION_STATUS","MARITAL_STATUS","AGE"],axis=1)
 cols = [ f for f in data.columns if data.dtypes[ f ] != "object"]
 cols.remove('NEXT_MONTH_DEFAULT')
-print(data)
 f = pd.melt( data, id_vars='NEXT_MONTH_DEFAULT',  value_vars=cols)
 g = sns.FacetGrid( f, hue='NEXT_MONTH_DEFAULT', col="variable", col_wrap=5, sharex=False, sharey=False )
 g = g.map( sns.distplot, "value", kde=True).add_legend()
@@ -85,10 +84,6 @@ def CSTOW ( inputdata, inputvariable, OutcomeCategory ):
     possibleValues = inputdata[inputvariable].unique()
     observed = []
     expected = []
-    print(OutcomeCategoryTable)
-    print(OutcomeCategoryRatio)
-    print(possibleValues)
-
 
     for possible in possibleValues:
         countsInCategories = inputdata[ inputdata[ inputvariable ] == possible ].groupby( OutcomeCategory )[OutcomeCategory].count().values
@@ -98,7 +93,7 @@ def CSTOW ( inputdata, inputvariable, OutcomeCategory ):
         elif( min(countsInCategories) < 5 ):
             print("Chi Squared Test needs at least 5 observations in each cell!")
             print( inputvariable + "=" + str(possible) + " has insufficient data")
-            print( countsInCategories )
+
             return
         else:
             observed.append( countsInCategories )
@@ -111,24 +106,21 @@ def CSTOW ( inputdata, inputvariable, OutcomeCategory ):
     p_value = 1 - stats.chi2.cdf(x=chi_squared_stat, df=degOfF)
     print("Calculated test-statistic is %.2f" % chi_squared_stat )
     print("If " + OutcomeCategory + " is indep of " + inputvariable + ", this has prob %.2e of occurring" % p_value )
-CSTOW(data,'eduF','NEXT_MONTH_DEFAULT')
+
 # The quantitative vars:
 quant = ["balF", "ageF"]
 # The qualitative but "Encoded" variables (ie most of them)
 qual_Enc = cols
 qual_Enc.remove("balF")
 qual_Enc.remove("ageF")
-print(cols)
 logged = []
 for m in ("PAID_AMT_JULY","PAID_AMT_AUG","PAID_AMT_SEP","PAID_AMT_OCT","PAID_AMT_NOV",'PAID_AMT_DEC'):
     qual_Enc.remove(m)
-    print(qual_Enc)
     data[m]  = data[m].apply( lambda x: np.log1p(x) if (x>0) else 0 )
     logged.append(m)
 
 for n in ("DUE_AMT_JULY","DUE_AMT_AUG","DUE_AMT_SEP","DUE_AMT_OCT","DUE_AMT_NOV","DUE_AMT_DEC"):
     qual_Enc.remove(n)
-    print(qual_Enc)
     data[ n] = data[n].apply( lambda x: np.log1p(x) if (x>0) else 0 )
     logged.append(n)
 
@@ -140,4 +132,76 @@ features = quant + qual_Enc + logged + ['NEXT_MONTH_DEFAULT']
 corr = data[features].corr()
 plt.subplots(figsize=(30,10))
 sns.heatmap( corr, square=True, annot=True, fmt=".1f" )
-plt.show()
+'''X_train=data.drop(["NEXT_MONTH_DEFAULT"],axis=1).values
+Y_train = data["NEXT_MONTH_DEFAULT"].values
+
+X_test = data.drop(["NEXT_MONTH_DEFAULT"],axis=1).values
+Y_test = data["NEXT_MONTH_DEFAULT"].values'''
+
+features = quant + qual_Enc + logged
+X = data[features].values
+Y = data[ "NEXT_MONTH_DEFAULT" ].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size=0.2)
+
+from sklearn.preprocessing import StandardScaler
+scX = StandardScaler()
+X_train = scX.fit_transform( X_train )
+X_test = scX.transform( X_test )
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=10)
+classifier.fit( X_train, Y_train )
+Y_pred = classifier.predict( X_test )
+
+cm = confusion_matrix( Y_test, Y_pred )
+print("Accuracy on Test Set for RandomForest = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
+scoresRF = cross_val_score( classifier, X_train, Y_train, cv=10)
+print("Mean RandomForest CrossVal Accuracy on Train Set %.2f, with std=%.2f" % (scoresRF.mean(), scoresRF.std() ))
+print(Y_pred)
+'''
+from sklearn.svm import SVC
+classifier1 = SVC(kernel="rbf")
+classifier1.fit( X_train, Y_train )
+Y_pred = classifier1.predict( X_test )
+
+cm = confusion_matrix( Y_test, Y_pred )
+print("Accuracy on Test Set for kernel-SVM = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
+scoresSVC = cross_val_score( classifier1, X_train, Y_train, cv=10)
+print("Mean kernel-SVM CrossVal Accuracy on Train Set %.2f, with std=%.2f" % (scoresSVC.mean(), scoresSVC.std() ))
+'''
+from sklearn.linear_model import LogisticRegression
+classifier2 = LogisticRegression()
+classifier2.fit( X_train, Y_train )
+Y_pred = classifier2.predict( X_test )
+
+cm = confusion_matrix( Y_test, Y_pred )
+print("Accuracy on Test Set for LogReg = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
+scoresLR = cross_val_score( classifier2, X_train, Y_train, cv=10)
+print("Mean LogReg CrossVal Accuracy on Train Set %.2f, with std=%.2f" % (scoresLR.mean(), scoresLR.std() ))
+
+from sklearn.naive_bayes import GaussianNB
+classifier3 = GaussianNB()
+classifier3.fit( X_train, Y_train )
+Y_pred = classifier3.predict( X_test )
+cm = confusion_matrix(Y_test, Y_pred )
+print("Accuracy on Test Set for NBClassifier = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
+scoresNB = cross_val_score( classifier3, X_train, Y_train, cv=10)
+print("Mean NaiveBayes CrossVal Accuracy on Train Set %.2f, with std=%.2f" % (scoresNB.mean(), scoresNB.std() ))
+
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier4 = KNeighborsClassifier(n_neighbors=5)
+classifier4.fit( X_train, Y_train )
+Y_pred = classifier4.predict( X_test )
+cm = confusion_matrix( Y_test, Y_pred )
+print("Accuracy on Test Set for KNeighborsClassifier = %.2f" % ((cm[0,0] + cm[1,1] )/len(X_test)))
+scoresKN = cross_val_score( classifier4, X_train, Y_train, cv=10)
+print("Mean KN CrossVal Accuracy on Train Set Set %.2f, with std=%.2f" % (scoresKN.mean(), scoresKN.std() ))
+
+df = pd.DataFrame(Y_pred)
+df.to_csv(r'a.csv')
